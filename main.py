edbc137ed7f0fb4bc31ba7118d229e60c56b2bf9
harshad2464 <patilharsh2464@gmail.com>
2024-08-22 15:16:32 +0530

    update port number

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ app.include_router(api_router)
 if __name__ == "__main__":
     import uvicorn
     # Use this for debugging purposes only       
-    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
     # uvicorn.main()
