e21bb6066b2bb7022cd1344ebd20b8eaeded5ead
harshad2464 <patilharsh2464@gmail.com>
2024-08-22 15:05:01 +0530

    add api urls file

--- app/api/api.py
+++ app/api/api.py
@@ -1,1 +1,8 @@
+from fastapi import APIRouter
+from app.api.endpoints import home
+from app.api.endpoints import crud_api
 
+api_router = APIRouter()
+
+api_router.include_router(home.router, prefix="/home", tags=["home"])
+api_router.include_router(crud_api.router, prefix="/crud_api", tags=["crud_api"])
